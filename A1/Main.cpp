4a8a050e0ea5fbda7cb2ae183d573e77c77491c7
Alfred Von <alfredvon@v1040-wn-rt-a-114-187.campus-dynamic.uwaterloo.ca>
2022-09-08 09:02:31 -0400

    first commit.
    Signed-off-by: Alfred Von <alfredvon@v1040-wn-rt-a-114-187.campus-dynamic.uwaterloo.ca>

---
+++ A1/Main.cpp
@@ -0,0 +1,9 @@
+// Termm--Fall 2022
+
+#include "A1.hpp"
+
+int main( int argc, char **argv ) 
+{
+	CS488Window::launch( argc, argv, new A1(), 1024, 768, "F22 Assignment 1" );
+	return 0;
+}
---
+++ A4/Material.hpp
@@ -0,0 +1,11 @@
+// Termm--Fall 2022
+
+#pragma once
+
+class Material {
+public:
+  virtual ~Material();
+
+protected:
+  Material();
+};
