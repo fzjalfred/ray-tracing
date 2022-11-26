f17d13207ce47e4b16b4002ac300b0e48e421b76
fzj <z63feng@uwaterloo.ca>
2022-11-26 02:37:06 -0500

    multithreading added.
    Signed-off-by: fzj <z63feng@uwaterloo.ca>

--- A5/A5.cpp
+++ A5/A5.cpp
@@ -10,16 +10,18 @@
 #include <tbb/parallel_for.h>
 #include <tbb/global_control.h>
 #include <chrono>
+#include <pthread.h>
 using namespace std::chrono;
 
 
 vec3 drakBlue = vec3(0.075, 0.1, 0.4);
 vec3 lightBlue = vec3(0.33, 0.42, 0.67);
 
-float mirror_reflect_coefficient = 0.9;
-
 int super_samples = 100;
+float mirror_reflect_coefficient = 0.9;
+int NUM_THREADS = 4;
 
+pthread_mutex_t counter_mutex_ = PTHREAD_MUTEX_INITIALIZER;
 using namespace std;
 
 vec3 phongModel(vec3 fragPosition, vec3 fragNormal, const Light& ray, const vec3& eye, Material& material, const vec3& ambient) {
@@ -125,6 +127,84 @@ vec3 tracing(Ray& ray, SceneNode* root,
 	return color;
 }
 
+struct thread_data {
+    int m;
+    int n;
+
+    std::vector<pair<uint, uint>>& tasks;
+    vec3 m_lowerLeftCorner;
+    vec3 x_axis;
+    vec3 y_axis;
+    size_t h;
+    Image& image;
+
+    SceneNode* root;
+    const glm::vec3& eye;
+    const glm::vec3& ambient;
+    const std::list<Light*>& lights;
+
+	uint& counter;
+	uint total;
+};
+
+void* f_task(void* threadarg) {
+    struct thread_data* my_data;
+    my_data = (struct thread_data*)threadarg;
+
+	int m = my_data->m;
+    int n = my_data->n;
+
+    std::vector<pair<uint, uint>>& tasks = my_data->tasks;
+    vec3 m_lowerLeftCorner = my_data->m_lowerLeftCorner;
+    vec3 x_axis = my_data->x_axis;
+    vec3 y_axis = my_data->y_axis;
+    size_t h = my_data->h;
+    Image& image = my_data->image;
+    uint& counter = my_data->counter;
+	uint total = my_data->total;
+
+    SceneNode* root = my_data->root;
+    const glm::vec3& eye = my_data->eye;
+    const glm::vec3& ambient = my_data->ambient;
+    const std::list<Light*>& lights = my_data->lights;
+
+    for (int i = m; i <= n; ++i) {
+        uint x = tasks[i].first;
+        uint y = tasks[i].second;
+        glm::vec3 color;
+#ifndef SUPER_SAMPLING
+        vec3 direction = m_lowerLeftCorner + x * x_axis + (h - y - 1) * y_axis;
+        Ray ray = Ray(eye, direction);
+        color += tracing(ray, root, eye, ambient, lights);
+#else
+        for (int sps = 0; sps < super_samples; ++sps) {
+            float u = static_cast<float>(x + drand48());
+            float v = static_cast<float>(y + drand48());
+            vec3 direction =
+                m_lowerLeftCorner + u * x_axis + (h - v - 1) * y_axis;
+            Ray ray = Ray(eye, direction);
+            color += tracing(ray, root, eye, ambient, lights);
+        }
+        color /= static_cast<float>(super_samples);
+#endif
+        // Red:
+        image(x, y, 0) = (double)color.r;
+        // Green:
+        image(x, y, 1) = (double)color.g;
+        // Blue:
+        image(x, y, 2) = (double)color.b;
+
+        counter++;
+        if (counter % 10000 == 0) {
+            cout << "rendering... (" << counter << ", " << total << ") ";
+            printf("%0.2f%%", counter / (float)total * 100);
+            cout << endl;
+        }
+
+    }
+
+    pthread_exit(NULL);
+};
 
 void A5_Render(
 		// What to render  
@@ -214,52 +294,73 @@ void A5_Render(
 		}
 	}
 #else
-	tbb::global_control c(tbb::global_control::max_allowed_parallelism, 4);
 	uint counter = 0;
-	tbb::parallel_for(tbb::blocked_range<int>(0, tasks.size()),
-		[&](tbb::blocked_range<int> r) {
-			for (int i = r.begin(); i < r.end(); ++i) {
-				uint x = tasks[i].first;
-				uint y = tasks[i].second;
-				glm::vec3 color;
-				#ifndef SUPER_SAMPLING
-				vec3 direction = m_lowerLeftCorner + x*x_axis + (h-y-1)*y_axis;
-				Ray ray = Ray(eye, direction);
-				color += tracing(ray, root, eye, ambient, lights);
-				#else
-				for (int sps = 0; sps < super_samples; ++sps)
-				{
-					float u = static_cast<float>(x + drand48());
-					float v = static_cast<float>(y + drand48());
-					vec3 direction = m_lowerLeftCorner + u*x_axis + (h-v-1)*y_axis;
-					Ray ray = Ray(eye, direction);
-					color += tracing(ray, root, eye, ambient, lights);
-				}
-				color /= static_cast<float>(super_samples);
-				#endif
-				// Red: 
-				image(x, y, 0) = (double)color.r;
-				// Green: 
-				image(x, y, 1) = (double)color.g;
-				// Blue: 
-				image(x, y, 2) = (double)color.b;
-				counter++;
-				if (counter%10000 == 0) {
-					cout<<"rendering... ("<<counter<<", "<<total<<") ";
-					printf("%0.2f%%",counter/(float)total*100);
-					cout<<endl;
-				}
-			}
-		});
+	pthread_t threads[NUM_THREADS];
+	struct thread_data* td[NUM_THREADS];
+	pthread_mutex_t count_mutex;
 
-	
-	std::cout << total << std::endl;
+	int rc;
+	for(int i = 0; i < NUM_THREADS; i++ ) {
+		int m = (i)*total/NUM_THREADS;
+		int n = (i+1)*total/NUM_THREADS - 1;
+		td[i] = new thread_data{m,n,tasks,m_lowerLeftCorner,x_axis,y_axis, h, image, root, eye, ambient, lights, counter, total};
+      	cout << "main() : creating thread, ranged from " << m << " " << n << endl;
+      	rc = pthread_create(&threads[i], NULL, f_task, (void *)td[i]);
+      
+      	if (rc) {
+        	 cout << "Error:unable to create thread," << rc << endl;
+        	 exit(-1);
+      	}
+   	}
+	for (int i = 0; i < NUM_THREADS; i++) {
+		pthread_join( threads[i], NULL);
+	}
+   	
 
 #endif
 	
     auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<seconds>(stop - start);
+	auto duration = duration_cast<microseconds>(stop - start);
 	std::cout<<"Time consumption: " << duration.count() <<" sec."<< std::endl;
 	
-
+	pthread_exit(NULL);
 }
+
+
+// tbb::global_control c(tbb::global_control::max_allowed_parallelism, 4);
+	
+	// tbb::parallel_for(tbb::blocked_range<int>(0, tasks.size()),
+	// 	[&](tbb::blocked_range<int> r) {
+	// 		for (int i = r.begin(); i < r.end(); ++i) {
+	// 			uint x = tasks[i].first;
+	// 			uint y = tasks[i].second;
+	// 			glm::vec3 color;
+	// 			#ifndef SUPER_SAMPLING
+	// 			vec3 direction = m_lowerLeftCorner + x*x_axis + (h-y-1)*y_axis;
+	// 			Ray ray = Ray(eye, direction);
+	// 			color += tracing(ray, root, eye, ambient, lights);
+	// 			#else
+	// 			for (int sps = 0; sps < super_samples; ++sps)
+	// 			{
+	// 				float u = static_cast<float>(x + drand48());
+	// 				float v = static_cast<float>(y + drand48());
+	// 				vec3 direction = m_lowerLeftCorner + u*x_axis + (h-v-1)*y_axis;
+	// 				Ray ray = Ray(eye, direction);
+	// 				color += tracing(ray, root, eye, ambient, lights);
+	// 			}
+	// 			color /= static_cast<float>(super_samples);
+	// 			#endif
+	// 			// Red: 
+	// 			image(x, y, 0) = (double)color.r;
+	// 			// Green: 
+	// 			image(x, y, 1) = (double)color.g;
+	// 			// Blue: 
+	// 			image(x, y, 2) = (double)color.b;
+	// 			counter++;
+	// 			if (counter%10000 == 0) {
+	// 				cout<<"rendering... ("<<counter<<", "<<total<<") ";
+	// 				printf("%0.2f%%",counter/(float)total*100);
+	// 				cout<<endl;
+	// 			}
+	// 		}
+	// 	});
--- A5/A5.hpp
+++ A5/A5.hpp
@@ -10,7 +10,11 @@
 
 
 #define MirrorReflection
-// #define TBB_MULTI_THREADING
+
+
+#define TBB_MULTI_THREADING
+
+
 #define SUPER_SAMPLING
 
 
